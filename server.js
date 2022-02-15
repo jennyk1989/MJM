a2dd832779abd33ca750c32120581f8fa4f8121d
jennyk1989 <92952780+jennyk1989@users.noreply.github.com>
2022-02-15 00:53:55 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 //connect to the db
-const sequelize = require('/config/connection');
+const sequelize = require('/Users/jenniferkeene/Documents/GitHub/MJM-Task-Master/config/connection.js');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -30,7 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers'));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
