499624853dd0b22124e55296af205fdb6de56c73
Jenny Keene <jennifer0keene@gmail.com>
2022-02-15 20:45:39 -0600

    fixed model's index

--- models/index.js
+++ models/index.js
@@ -1,18 +1,17 @@
-const seedCategories = require('./category-seeds');
-const seedTasks = require('./task-seeds');
+// import models
+const Task = require('./Task');
+const Category = require('./Category');
 
-const sequelize = require('../config/connection');
+Task.belongsTo(Category, { 
+  foreignKey: 'category_id'
+});
 
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+Category.hasMany(Task, {
+  foreignKey: 'category_id' 
+});
 
-  await seedTasks();
-  console.log('\n----- TASKS SEEDED -----\n');
-
-  process.exit(0);
+module.exports = {
+  Task,
+  Category,
 };
 
-seedAll();
