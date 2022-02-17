b69f93dc735eece6be08c63e32a053d423657a71
Jenny Keene <jennifer0keene@gmail.com>
2022-02-17 12:45:11 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -4,41 +4,13 @@ require('dotenv').config();
 
 let sequelize;
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else if (process.env.DATABASE_URL) {
-  const Sequelize = require('sequelize');
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
-);
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    logging: true
   });
 }
 
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 
-const PORT = process.env.PORT || 8080;
 const app = express();
-app.set("port", PORT);
+const PORT = process.env.PORT || 3001;
+
 // parse requests in JSON format
 app.use(express.json()); 
 // parse requests in url format
@@ -35,8 +35,6 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => { 
-  app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+sequelize.sync({ force: true }).then(() => { 
+  app.listen(PORT, () => console.log('App now listening!'));
 });
