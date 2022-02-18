bdf9734e4fc142f3b7cb6c19d24c68186eb39c77
jennyk1989 <jennifer0keene@gmail.com>
2022-02-17 19:09:33 -0600

    Update task-seeds.js

--- seeds/task-seeds.js
+++ seeds/task-seeds.js
@@ -16,6 +16,15 @@ const taskData = [
   {
     task_name: 'Fix gate'
   },
+  {
+    task_name: 'Mow the lawn'
+  },
+  {
+    task_name: 'Buy new computer'
+  },
+  {
+    task_name: 'Go running'
+  }
 ];
 
 const seedTasks = () => Task.bulkCreate(taskData);
