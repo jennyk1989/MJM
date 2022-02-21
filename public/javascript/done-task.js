f6dbf474f701596f342a7caf9dabb4fd5f06910e
Jenny Keene <jennifer0keene@gmail.com>
2022-02-21 00:03:27 -0600

    confetti when done is clicked

--- public/javascript/done-task.js
+++ public/javascript/done-task.js
@@ -5,9 +5,7 @@ async function taskIsDone(event) {
     let idstring = grandpa.children[1].id;
     let id = parseInt(idstring);
     let task_name = grandpa.children[1].innerHTML;
-    console.log(task_name);
-    console.log(grandpa);
-    console.log(id);
+    
     const response = await fetch(`/api/tasks`, {
         method: 'DELETE',
         body: JSON.stringify({
